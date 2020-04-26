d575a078e54ea18b1cbb3ea82570e9fcc57c63a0
nouf <noufalmedlej@gmail.com>
2020-04-26 22:33:39 +0300

    reset password and register interface fixed

--- app/src/main/java/com/example/myhealthbuddy/ResetPassword.java
+++ app/src/main/java/com/example/myhealthbuddy/ResetPassword.java
@@ -27,16 +27,7 @@ public class ResetPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resetpassword);
 
-        BottomNavigationView bottomnav;
-        bottomnav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomnav.setSelectedItemId(R.id.nav_person);
-        bottomnav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                UserMenuSelector(menuItem);
-                return false;
-            }
-        });
+
         mail = findViewById(R.id.mailll);
         Reset = findViewById(R.id.reset);
 
@@ -45,23 +36,21 @@ public class ResetPassword extends AppCompatActivity {
         Reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Email =mail.getText().toString();
+                String Email = mail.getText().toString();
 
-                if (Email.isEmpty()){
-                    Toast.makeText(getApplicationContext(),"ادخل بريدك الإلكتروني",Toast.LENGTH_LONG).show();
-                }
-                else{
+                if (Email.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "ادخل بريدك الإلكتروني", Toast.LENGTH_LONG).show();
+                } else {
                     mAuth.sendPasswordResetEmail(Email).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()){
-                                Toast.makeText(getApplicationContext(),"لقد تم ارسال رسالة تغيير كلمة المرور إلى بريدك الإلكتروني",Toast.LENGTH_LONG).show();
-                                startActivity(new Intent(ResetPassword.this,SigninActivity.class));
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "لقد تم ارسال رسالة تغيير كلمة المرور إلى بريدك الإلكتروني", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(ResetPassword.this, SigninActivity.class));
 
-                            }
-                            else {
+                            } else {
                                 String Error = task.getException().getMessage();
-                                Toast.makeText(getApplicationContext(),Error,Toast.LENGTH_LONG).show();
+                                Toast.makeText(getApplicationContext(), Error, Toast.LENGTH_LONG).show();
 
                             }
                         }
@@ -70,15 +59,5 @@ public class ResetPassword extends AppCompatActivity {
             }
         });
     }
-    private void UserMenuSelector(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.nav_person:
-                Intent intentsearchh=new Intent(ResetPassword.this, Profile.class);
-                startActivity(intentsearchh);
-                break;
-            case R.id.nav_home:
-                Intent intentsearch=new Intent(ResetPassword.this, HomePage.class);
-                startActivity(intentsearch);
-                break;}}
 }
 
